a7492f424f4d02d38c7a111fcff8921af65ac203
yyyy-01 <100837114+yyyy-01@users.noreply.github.com>
2024-03-23 15:42:38 +0800

    Initial commit: Setup Flame and Assets

---
+++ android/app/src/main/kotlin/com/thengyuanyuan/flutter_flappy_bird_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.thengyuanyuan.flutter_flappy_bird_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+import 'game/flappy_bird_game.dart';
+
+void main() {
+  final game = FlappyBirdGame();
+  runApp(
+    GameWidget(game: game),
+  );
+}
